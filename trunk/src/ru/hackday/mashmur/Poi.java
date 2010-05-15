62eeb172e92695123a2ac22f5d75a77405c61287
ildar.karimov <ildar.karimov@9905b6cc-ff92-d7ec-82b4-05e3af3e3681>
2010-05-15 15:19:59 +0000

    poi constructor and getters/setters

--- trunk/src/ru/hackday/mashmur/Poi.java
+++ trunk/src/ru/hackday/mashmur/Poi.java
@@ -11,6 +11,49 @@ public class Poi {
     String name;
     String audioUrl;
 
+    public Poi() {
+    }
+
+    public int getLatitudeE6() {
+        return latitudeE6;
+    }
+
+    public void setLatitudeE6(int latitudeE6) {
+        this.latitudeE6 = latitudeE6;
+    }
+
+    public int getLongitudeE6() {
+        return longitudeE6;
+    }
+
+    public void setLongitudeE6(int longitudeE6) {
+        this.longitudeE6 = longitudeE6;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAudioUrl() {
+        return audioUrl;
+    }
+
+    public void setAudioUrl(String audioUrl) {
+        this.audioUrl = audioUrl;
+    }
+
     public Poi(int latitudeE6, int longitudeE6, String description, String name, String audioUrl) {
         this.latitudeE6 = latitudeE6;
         this.longitudeE6 = longitudeE6;
