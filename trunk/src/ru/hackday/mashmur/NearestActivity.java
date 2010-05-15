eed4c551cba6a91d5cb8ea76039abeeedddaecc5
ildar.karimov <ildar.karimov@9905b6cc-ff92-d7ec-82b4-05e3af3e3681>
2010-05-15 12:12:34 +0000

    simple map overlay

--- trunk/src/ru/hackday/mashmur/NearestActivity.java
+++ trunk/src/ru/hackday/mashmur/NearestActivity.java
@@ -1,18 +1,44 @@
 package ru.hackday.mashmur;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import com.google.android.maps.MapActivity;
+import android.widget.LinearLayout;
+import android.widget.ZoomControls;
+import com.google.android.maps.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NearestActivity extends MapActivity {
+    LinearLayout linearLayout;
+    MapView mapView;
+    ZoomControls zoomControls;
+
+    List<Overlay> mapOverlays;
+    Drawable drawable;
+    NearestOverlay itemizedOverlay;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_view);
-        
+        linearLayout = (LinearLayout) findViewById(R.id.zoomview);
+        mapView = (MapView) findViewById(R.id.mapview);
+        zoomControls = (ZoomControls) mapView.getZoomControls();
+        linearLayout.addView(zoomControls);
+
+        mapOverlays = mapView.getOverlays();
+        drawable = this.getResources().getDrawable(R.drawable.androidmarker);
+        itemizedOverlay = new NearestOverlay(drawable);
+
+        GeoPoint point = new GeoPoint(19240000, -99120000);
+        OverlayItem overlayitem = new OverlayItem(point, "point1", "point2");
+        itemizedOverlay.addOverlay(overlayitem);
+        mapOverlays.add(itemizedOverlay);
     }
 
     @Override
     protected boolean isRouteDisplayed() {
-        return false;  //To change body of implemented methods use File | Settings | File Templates.
+        return false;
     }
 }
