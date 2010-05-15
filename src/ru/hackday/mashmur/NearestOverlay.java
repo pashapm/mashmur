217ec121f2399cd524134f4ae6abf97ccfc0fc20
ildar.karimov <ildar.karimov@9905b6cc-ff92-d7ec-82b4-05e3af3e3681>
2010-05-15 13:04:21 +0000

    track overlay

--- src/ru/hackday/mashmur/NearestOverlay.java
+++ src/ru/hackday/mashmur/NearestOverlay.java
@@ -1,5 +1,7 @@
 package ru.hackday.mashmur;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.OverlayItem;
@@ -9,9 +11,11 @@ import java.util.ArrayList;
 public class NearestOverlay extends ItemizedOverlay {
 
     private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+    private Context context;
 
-    public NearestOverlay(Drawable drawable) {
+    public NearestOverlay(Drawable drawable, Context context) {
         super(boundCenterBottom(drawable));
+        this.context = context;
     }
 
     @Override
@@ -28,5 +32,16 @@ public class NearestOverlay extends ItemizedOverlay {
     protected OverlayItem createItem(int i) {
         return mOverlays.get(i);
     }
+
+    @Override
+    protected boolean onTap(int index) {
+        OverlayItem item = mOverlays.get(index);
+        AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+        dialog.setTitle(item.getTitle());
+        dialog.setMessage(item.getSnippet());
+        dialog.show();
+        return true;
+    }
+
 
 }
