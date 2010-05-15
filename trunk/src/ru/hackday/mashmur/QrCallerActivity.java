65239ee69f099d6d766e4f843b7b0edf27811b3d
jecklandin <jecklandin@9905b6cc-ff92-d7ec-82b4-05e3af3e3681>
2010-05-15 10:53:47 +0000

    initial grid

---
+++ trunk/src/ru/hackday/mashmur/QrCallerActivity.java
@@ -0,0 +1,13 @@
+package ru.hackday.mashmur;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QrCallerActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    
+}
