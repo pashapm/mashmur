8849b09ed20a90e7317a01b834d467528a3daac9
ildar.karimov <ildar.karimov@9905b6cc-ff92-d7ec-82b4-05e3af3e3681>
2010-05-16 10:03:58 +0000

    added empty PoiShowActivity

---
+++ trunk/src/ru/hackday/mashmur/PoiShowActivity.java
@@ -0,0 +1,11 @@
+package ru.hackday.mashmur;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PoiShowActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
