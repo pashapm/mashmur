37ab32c0569e82758cba56cb6b518cdb1de50a2a
jecklandin <jecklandin@9905b6cc-ff92-d7ec-82b4-05e3af3e3681>
2010-05-16 11:04:26 +0000

    handing poi to PoiShowActivity

--- src/ru/hackday/mashmur/PoiShowActivity.java
+++ src/ru/hackday/mashmur/PoiShowActivity.java
@@ -2,10 +2,21 @@ package ru.hackday.mashmur;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class PoiShowActivity extends Activity {
+	
+	Poi mPoi;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.poi_item);
+        mPoi = getIntent().getExtras().getParcelable("poi");
+        TextView name = (TextView) findViewById(R.id.name);
+        name.setText(mPoi.getName());
+        TextView desc = (TextView) findViewById(R.id.desc);
+        desc.setText(mPoi.getDescription());
     }
 }
