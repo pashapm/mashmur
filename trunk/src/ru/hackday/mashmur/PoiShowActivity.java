e343676f88e39d3f3d4d5033d3e7c5df0dbcce75
Anastasiya.Dremina <Anastasiya.Dremina@9905b6cc-ff92-d7ec-82b4-05e3af3e3681>
2010-05-16 13:40:33 +0000

    temp fix

--- trunk/src/ru/hackday/mashmur/PoiShowActivity.java
+++ trunk/src/ru/hackday/mashmur/PoiShowActivity.java
@@ -1,7 +1,10 @@
 package ru.hackday.mashmur;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,13 +12,31 @@ import android.widget.TextView;
 import ru.hackday.mashmur.media.MediaPlayerDemo;
 import ru.hackday.mashmur.media.MediaPlayerDemo_Audio;
 
-public class PoiShowActivity extends Activity {
+import java.awt.image.SampleModel;
 
+public class PoiShowActivity extends Activity {
+    CompassView compassView;
+    private SensorManager mSensorManager;
     Poi mPoi;
+    private final SensorListener mListener = new SensorListener() {
+
+        public void onSensorChanged(int sensor, float[] values) {
+            CompassView.mValues = values;
+            if (compassView != null) {
+                compassView.invalidate();
+            }
+        }
+
+        public void onAccuracyChanged(int sensor, int accuracy) {
+            // TODO Auto-generated method stub
+
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         setContentView(R.layout.poi_item);
         mPoi = getIntent().getExtras().getParcelable("poi");
         TextView name = (TextView) findViewById(R.id.name);
@@ -34,7 +55,22 @@ public class PoiShowActivity extends Activity {
                 startActivity(intent);
             }
         }
-
         );
+        compassView = (CompassView) findViewById(R.id.compass);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mListener,
+                SensorManager.SENSOR_ORIENTATION,
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onStop() {
+        mSensorManager.unregisterListener(mListener);
+        super.onStop();
+    }
+
 }
