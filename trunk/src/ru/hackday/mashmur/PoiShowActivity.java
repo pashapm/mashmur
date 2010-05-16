2abe2ebfa1fd736de3980c52093c70ea9f57fdf6
Anastasiya.Dremina <Anastasiya.Dremina@9905b6cc-ff92-d7ec-82b4-05e3af3e3681>
2010-05-16 13:21:21 +0000

    cosmetic changes

--- trunk/src/ru/hackday/mashmur/PoiShowActivity.java
+++ trunk/src/ru/hackday/mashmur/PoiShowActivity.java
@@ -1,13 +1,18 @@
 package ru.hackday.mashmur;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import ru.hackday.mashmur.media.MediaPlayerDemo;
+import ru.hackday.mashmur.media.MediaPlayerDemo_Audio;
 
 public class PoiShowActivity extends Activity {
-	
-	Poi mPoi;
-	
+
+    Poi mPoi;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +22,19 @@ public class PoiShowActivity extends Activity {
         name.setText(mPoi.getName());
         TextView desc = (TextView) findViewById(R.id.desc);
         desc.setText(mPoi.getDescription());
+        Button playButton = (Button) findViewById(R.id.play);
+        playButton.setVisibility(View.VISIBLE);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent =
+                        new Intent(PoiShowActivity.this.getApplication(),
+                                MediaPlayerDemo_Audio.class);
+                //todo set uri for audio
+                intent.putExtra(MediaPlayerDemo.EXTRA_AUDIO_URI, mPoi.audioUrl);
+                startActivity(intent);
+            }
+        }
+
+        );
     }
 }
